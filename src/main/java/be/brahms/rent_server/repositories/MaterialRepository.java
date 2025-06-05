151c30b589169efbeba8c73782729a0dfae72044
Brahms <34168107+bramsobrahms@users.noreply.github.com>
2025-06-05 21:07:29 +0200

    Comment all repositories

--- src/main/java/be/brahms/rent_server/repositories/MaterialRepository.java
+++ src/main/java/be/brahms/rent_server/repositories/MaterialRepository.java
@@ -4,6 +4,10 @@ import be.brahms.rent_server.models.entities.Material;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository for managing Material entities.
+ * Provides basic CRUD operations and more using JpaRepository.
+ */
 @Repository
 public interface MaterialRepository extends JpaRepository<Material, Long> {
 }
--- src/main/java/be/brahms/rent_server/repositories/ReviewRepository.java
+++ src/main/java/be/brahms/rent_server/repositories/ReviewRepository.java
@@ -4,6 +4,10 @@ import be.brahms.rent_server.models.entities.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository for managing Review entities.
+ * Provides basic CRUD operations and more using JpaRepository.
+ */
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
 }
