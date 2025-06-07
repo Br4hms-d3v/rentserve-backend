54ef54e513824b6bb3b0cf0fbae3cec909f37014
B K <user.name>
2025-06-08 01:45:21 +0200

    Config OpenApi

---
+++ src/main/java/be/brahms/rent_server/configurations/swagger/OpenApiConfig.java
@@ -0,0 +1,38 @@
+package be.brahms.rent_server.configurations.swagger;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration class for OpenAPI (Swagger) documentation.
+ * This sets up JWT Bearer authentication for the API.
+ */
+@Configuration
+public class OpenApiConfig {
+
+    /**
+     * Creates and configures the OpenAPI documentation.
+     * Adds JWT Bearer authentication as a security scheme.
+     *
+     * @return the custom OpenAPI configuration
+     */
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "BearerAuth"; // The name of token
+
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName)) // Need a token to be used
+                .components(
+                        new Components().addSecuritySchemes(securitySchemeName, // Explain how the security works and use the same name
+                                new SecurityScheme()
+                                        .name(securitySchemeName) // The name of security
+                                        .type(SecurityScheme.Type.HTTP) // Type of web
+                                        .scheme("bearer") // The token bearer
+                                        .bearerFormat("JWT")) // the token format is JWT
+                );
+    }
+}
