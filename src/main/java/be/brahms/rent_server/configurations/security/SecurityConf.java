a4ea84738f9055498b8e74d37aaf84e4ba9f8c74
B K <user.name>
2025-06-08 18:55:08 +0200

    Update JWTFilter for javadoc

--- src/main/java/be/brahms/rent_server/configurations/security/SecurityConf.java
+++ src/main/java/be/brahms/rent_server/configurations/security/SecurityConf.java
@@ -2,7 +2,10 @@ package be.brahms.rent_server.configurations.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * This class configures security settings for the application.
@@ -24,6 +27,24 @@ public class SecurityConf {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * Creates the security filter chain.
+     * This method disables CSRF and CORS protection.
+     * It allows all requests without restriction.
+     *
+     * @param http the HttpSecurity to configure
+     * @return the built SecurityFilterChain
+     * @throws Exception if an error occurs during configuration
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request.anyRequest().permitAll());
+
+        return http.build();
     }
 
 }
