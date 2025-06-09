6c419040b4e3442d7cfed637a2b671cc0e96371d
B K <user.name>
2025-06-09 19:10:12 +0200

    Create a registre user

--- src/main/java/be/brahms/rent_server/services/UserService.java
+++ src/main/java/be/brahms/rent_server/services/UserService.java
@@ -1,8 +1,19 @@
 package be.brahms.rent_server.services;
 
+import be.brahms.rent_server.models.entities.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 /**
  * Service interface for managing user.
  * Defines business operations related to Bill entities.
  */
-public interface UserService {
+public interface UserService extends UserDetailsService {
+
+    /**
+     * This method saves a new user.
+     *
+     * @param user the user to register
+     * @return the saved user
+     */
+    User register(User user);
 }
--- src/main/java/be/brahms/rent_server/services/impl/UserServiceImpl.java
+++ src/main/java/be/brahms/rent_server/services/impl/UserServiceImpl.java
@@ -1,7 +1,13 @@
 package be.brahms.rent_server.services.impl;
 
+import be.brahms.rent_server.enums.Role;
+import be.brahms.rent_server.models.entities.User;
 import be.brahms.rent_server.repositories.UserRepository;
 import be.brahms.rent_server.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -12,13 +18,55 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     /**
      * Constructor to create UserServiceImpl with UserRepository.
      *
      * @param userRepository the repository to access user data
      */
-    public UserServiceImpl(UserRepository userRepository) {
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    /**
+     * This method registers a new user.
+     * It sets the role to MEMBER, hashes the password,
+     * and makes the user not active.
+     * It also makes a token (not used here).
+     * Then it saves the user.
+     *
+     * @param user the user to register
+     * @return the saved user
+     */
+    @Override
+    public User register(User user) {
+        // User start has a member
+        user.setRole(Role.MEMBER);
+        // Hash the password
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        // User is not active but it is created
+        user.setIsActive(false);
+
+        // Generate a token
+        String registrationToken = "passwordToken";
+
+        return userRepository.save(user);
+    }
+
+    // It is from by UseDetailService
+    /**
+     * This method finds a user by username.
+     * Now, it returns nothing (null).
+     *
+     * @param username the username to find
+     * @return the user details or null
+     * @throws UsernameNotFoundException if user is not found
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
     }
 }
