1fb8fca6592ff9ad67181a374291c1d88855008f
Brahms <bramso.brahms@gmail.com>
2025-07-25 02:29:52 +0200

    Create CategoriesController

---
+++ src/main/java/be/brahms/rent_serve/controllers/CategoriesController.java
@@ -0,0 +1,59 @@
+package be.brahms.rent_serve.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/categorie")
+public class CategoriesController {
+
+    /**
+     * List of all categories
+     * */
+    public ResponseEntity<> {
+    }
+
+    /**
+     * Create a new category
+     * */
+    public ResponseEntity<> {
+    }
+
+    /**
+     * Edit category
+     * */
+    public ResponseEntity<> {
+    }
+
+    /**
+     * Delete category
+     * */
+    public ResponseEntity<> {
+    }
+
+    /**
+     * Select all categories by material
+     * */
+    public ResponseEntity<> {
+    }
+
+    /**
+     * Select all categories by favor
+     * */
+    public ResponseEntity<> {
+    }
+
+    /**
+     * Search categories for material
+     * */
+    public ResponseEntity<> {
+    }
+
+    /**
+     * Search categories for favor
+     * */
+    public ResponseEntity<> {
+    }
+
+}
