5f2eb61bf1bcd1b08e87e697b6c1600d42b9693e
Brahms <bramso.brahms@gmail.com>
2025-08-13 16:44:42 +0200

    add javaDoc

--- src/main/java/be/brahms/rent_serve/models/dtos/material/MaterialDto.java
+++ src/main/java/be/brahms/rent_serve/models/dtos/material/MaterialDto.java
@@ -8,6 +8,17 @@ import be.brahms.rent_serve.models.entities.UserMaterial;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * A Data Transfer Object (DTO) for material information.
+ * It contains simple data about a material and its details.
+ *
+ * @param id                  The unique identifier
+ * @param nameMaterial        The name of material
+ * @param descriptionMaterial The description explain detail of material
+ * @param priceMaterial       The price per hour
+ * @param stateMaterial       The state of material
+ * @param picture             The picture to show the material
+ */
 public record MaterialDto(
         long id,
         String nameMaterial,
@@ -17,13 +28,20 @@ public record MaterialDto(
         String picture
 ) {
 
-    // Convert one Material + UserMaterial into a DTO
+    /**
+     * Create a MaterialDto from one Material and one UserMaterial.
+     *
+     * @param material     the main material object
+     * @param userMaterial the user-specific material details
+     * @return a new MaterialDto with data from both objects
+     */
     public static MaterialDto fromEntity(Material material, UserMaterial userMaterial) {
 
         String descriptionMaterial = userMaterial.getDescriptionMaterial();
         double priceMaterial = userMaterial.getPriceHourMaterial().doubleValue();
         State stateMaterial = userMaterial.getStateMaterial();
 
+        // Display only one picture
         String picture = userMaterial.getPictures()
                 .stream()
                 .findFirst()
@@ -41,7 +59,13 @@ public record MaterialDto(
         );
     }
 
-    // Convert one Material into List<MaterialDto>, one DTO per UserMaterial
+    /**
+     * Create a list of MaterialDto from one Material.
+     * One MaterialDto is created for each UserMaterial inside the Material.
+     *
+     * @param material the main material object
+     * @return a list of MaterialDto, one for each UserMaterial
+     */
     public static List<MaterialDto> listFromMaterial(Material material) {
         return material.getUserMaterials()
                 .stream()
--- src/main/java/be/brahms/rent_serve/services/impl/MaterialServiceImpl.java
+++ src/main/java/be/brahms/rent_serve/services/impl/MaterialServiceImpl.java
@@ -1,6 +1,5 @@
 package be.brahms.rent_serve.services.impl;
 
-import be.brahms.rent_serve.exceptions.material.MaterialException;
 import be.brahms.rent_serve.models.entities.Material;
 import be.brahms.rent_serve.repositories.MaterialRepository;
 import be.brahms.rent_serve.services.MaterialService;
@@ -25,18 +24,6 @@ public class MaterialServiceImpl implements MaterialService {
     public MaterialServiceImpl(MaterialRepository materialRepository) {
         this.materialRepository = materialRepository;
     }
-
-//    @Override
-//    public List<Material> findAllMaterials() {
-//
-//        List<Material> materials = materialRepository.findAll();
-//
-//        // If the list of materials are empty send a message
-//        if (materials.isEmpty()) {
-//            throw new MaterialException("la liste est vide");
-//        }
-//        return materials;
-//    }
 
     @Override
     public List<Material> findAllMaterials() {
