0c7f19fc2f222f09d17fa6a14baaee8d401a129f
Brahms <bramso.brahms@gmail.com>
2025-09-10 21:48:38 +0200

    list of userMaterial

--- src/main/java/be/brahms/rent_serve/services/UserMaterialService.java
+++ src/main/java/be/brahms/rent_serve/services/UserMaterialService.java
@@ -1,8 +1,16 @@
 package be.brahms.rent_serve.services;
 
+import be.brahms.rent_serve.models.entities.UserMaterial;
+
+import java.util.List;
+
 /**
  * Service interface for managing user material.
  * Defines business operations related to Bill entities.
  */
 public interface UserMaterialService {
+
+    List<UserMaterial> findAllUserMaterials();
+
+
 }
--- src/main/java/be/brahms/rent_serve/services/impl/UserMaterialServiceImpl.java
+++ src/main/java/be/brahms/rent_serve/services/impl/UserMaterialServiceImpl.java
@@ -1,8 +1,12 @@
 package be.brahms.rent_serve.services.impl;
 
+import be.brahms.rent_serve.exceptions.userMaterial.UserMaterialException;
+import be.brahms.rent_serve.models.entities.UserMaterial;
 import be.brahms.rent_serve.repositories.UserMaterialRepository;
 import be.brahms.rent_serve.services.UserMaterialService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Service implementation for managing user material.
@@ -21,4 +25,15 @@ public class UserMaterialServiceImpl implements UserMaterialService {
     public UserMaterialServiceImpl(UserMaterialRepository userMaterialRepository) {
         this.userMaterialRepository = userMaterialRepository;
     }
+
+    public List<UserMaterial> findAllUserMaterials() {
+        List<UserMaterial> userMaterials = userMaterialRepository.findAll();
+
+        if (userMaterials.isEmpty()) {
+            throw new UserMaterialException("La liste est vide");
+        }
+
+        return userMaterials;
+    }
+
 }
