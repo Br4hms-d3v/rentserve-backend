f002c91b51dd8d1b4d78e682b4026da79be59138
Brahms <bramso.brahms@gmail.com>
2025-09-10 21:51:00 +0200

    exception UserMaterial

---
+++ src/main/java/be/brahms/rent_serve/exceptions/userMaterial/UserMaterialException.java
@@ -0,0 +1,8 @@
+package be.brahms.rent_serve.exceptions.userMaterial;
+
+public class UserMaterialException extends RuntimeException {
+
+    public UserMaterialException(String message) {
+        super(message);
+    }
+}
