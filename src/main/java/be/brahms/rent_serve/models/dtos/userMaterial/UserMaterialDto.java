d91a8550d387dc74714d691c6493161fa0153075
Brahms <bramso.brahms@gmail.com>
2025-09-10 21:54:21 +0200

    userMaterial DTO

---
+++ src/main/java/be/brahms/rent_serve/models/dtos/userMaterial/UserMaterialDto.java
@@ -0,0 +1,29 @@
+package be.brahms.rent_serve.models.dtos.userMaterial;
+
+import be.brahms.rent_serve.enums.State;
+import be.brahms.rent_serve.models.entities.Picture;
+import be.brahms.rent_serve.models.entities.UserMaterial;
+
+import java.math.BigDecimal;
+
+public record UserMaterialDto(
+        State stateMaterial,
+        BigDecimal priceHourMaterial,
+        Boolean isAvailable,
+        String picture
+) {
+    public static UserMaterialDto fromEntity(UserMaterial userMaterial) {
+        String firstPicture = userMaterial.getPictures()
+                .stream()
+                .map(Picture::getNamePicture)
+                .findFirst()
+                .orElse(null);
+
+        return new UserMaterialDto(
+                userMaterial.getStateMaterial(),
+                userMaterial.getPriceHourMaterial(),
+                userMaterial.isAvailable(),
+                firstPicture
+        );
+    }
+}
